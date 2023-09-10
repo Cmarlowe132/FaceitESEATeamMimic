8fcbf843482fd7b23e112b233c2c4750902fde22
Carter Marlowe <marlowc@email.sc.edu>
2023-09-10 15:14:59 -0400

    Creating the HTML element and prepending it

--- TeamExtension.js
+++ TeamExtension.js
@@ -1,3 +1,9 @@
-if (document.getElementsByClassName("col-md-4")[0] !== null) {
-  const playerRightSideDetails = document.getElementsByClassName("col-md-4")[0];
-}
+(() => {
+  if (document.getElementsByClassName("col-md-4")[0] !== null) {
+    const playerRightSideDetails =
+      document.getElementsByClassName("col-md-4")[0];
+    const teamStatSection = document.createElement("div");
+    teamStatSection.className();
+    playerRightSideDetails.prepend(teamStatSection);
+  }
+})();
