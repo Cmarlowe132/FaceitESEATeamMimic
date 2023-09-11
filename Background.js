576a183cbfbbe268868b99e82122cc037ca08973
Carter Marlowe <marlowc@email.sc.edu>
2023-09-11 01:17:56 -0400

    Working on sending player name to content script

--- Background.js
+++ Background.js
@@ -1,8 +1,9 @@
 browser.tabs.onUpdated.addListener((tabId, tab) => {
+  console.log("yo");
   if (tab.url && tab.url.includes("players")) {
     const urlName = tab.url.split("players/")[1];
     const playerName = urlName.indexOf("/");
-
+    console.log("yo");
     browser.runtime.sendMessage(tabId, {
       playerName: playerName,
     });
