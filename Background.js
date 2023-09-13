947f68f6c4a0bc5ff83b34b742e61342bc2b1ed3
Carter Marlowe <marlowc@email.sc.edu>
2023-09-12 21:01:41 -0400

    Fixing some problems with sending messages

--- Background.js
+++ Background.js
@@ -1,11 +1,12 @@
 browser.tabs.onUpdated.addListener((tabId, tab) => {
-  console.log("yo");
   if (tab.url && tab.url.includes("players")) {
     const urlName = tab.url.split("players/")[1];
-    const playerName = urlName.indexOf("/");
-    console.log("yo");
-    browser.runtime.sendMessage(tabId, {
-      playerName: playerName,
-    });
+    if (!urlName.includes("/")) {
+      const playerName = urlName;
+      console.log(playerName);
+      browser.runtime.sendMessage({
+        playerName: playerName,
+      });
+    }
   }
 });
--- TeamExtension.js
+++ TeamExtension.js
@@ -1,15 +1,17 @@
 (() => {
-  const displayInfo = () => {
-    const playerRightSideDetails =
-      document.getElementsByClassName("col-md-4")[0];
-    const teamStatSection = document.createElement("div");
-    teamStatSection.innerHTML = "Hello";
-    playerRightSideDetails.prepend(teamStatSection);
-  };
-
-  browser.runtime.onMessage.addListener((obj, sender) => {
+  browser.runtime.onMessage.addListener((obj, sender, response) => {
+    console.log("HELLO");
     if (obj.playerName != null) {
       displayInfo();
     }
   });
+
+  const displayInfo = async () => {
+    const playerRightSideDetails =
+      document.getElementsByClassName("col-md-4")[0];
+    const teamStatSection = document.createElement("div");
+    console.log("here");
+    teamStatSection.innerHTML = "Hello";
+    playerRightSideDetails.prepend(teamStatSection);
+  };
 })();
