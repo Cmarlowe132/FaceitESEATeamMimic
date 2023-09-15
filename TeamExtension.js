2454be6e77343ab4102fa4f4f58a386c6cc386cc
Carter Marlowe <marlowc@email.sc.edu>
2023-09-15 16:49:19 -0400

    creating function that only runs on webpage fully loaded

--- TeamExtension.js
+++ TeamExtension.js
@@ -1,24 +1,25 @@
-(() => {
+browser.runtime.onMessage.addListener((obj, sender, response) => {
+  console.log("HELLO");
+  if (obj.playerName != null) {
+    displayInfo();
+  }
+});
+
+const displayInfo = async () => {
+  const playerRightSideDetails = document.getElementsByClassName("col-md-4")[0];
+  const teamStatSection = document.createElement("div");
+  teamStatSection.innerHTML = "Hello";
+  playerRightSideDetails.prepend(teamStatSection);
+  console.log("I have made it here");
+};
+
+const tempFuntion = async () => {
   document.body.style.border = "5px solid red";
+};
 
-  browser.runtime.onMessage.addListener((obj, sender, response) => {
-    console.log("HELLO");
-    if (obj.playerName != null) {
-      displayInfo();
-    }
-  });
-
-  const displayInfo = async () => {
-    const playerRightSideDetails =
-      document.getElementsByClassName("col-md-4")[0];
-    const teamStatSection = document.createElement("div");
-    console.log("here");
-    teamStatSection.innerHTML = "Hello";
-    playerRightSideDetails.prepend(teamStatSection);
-    return true;
-  };
-
-  const tempFuntion = async () => {
-    document.body.style.border = "5px solid red";
-  };
-})();
+document.addEventListener("DOMContentLoaded", (event) => {
+  console.log("im gonna blow my head off");
+});
+document.body.style.border = "5px solid red";
+console.log("Help me Please");
+displayInfo();
