086fbc45b9124f6c69e3b9b17cd5643d78225e4b
Carter Marlowe <marlowc@email.sc.edu>
2023-09-17 21:05:49 -0400

    Made it so content script waits for page to load

--- TeamExtension.js
+++ TeamExtension.js
@@ -5,21 +5,20 @@ browser.runtime.onMessage.addListener((obj, sender, response) => {
   }
 });
 
-const displayInfo = async () => {
-  const playerRightSideDetails = document.getElementsByClassName("col-md-4")[0];
-  const teamStatSection = document.createElement("div");
-  teamStatSection.innerHTML = "Hello";
-  playerRightSideDetails.prepend(teamStatSection);
-  console.log("I have made it here");
+const displayInfo = () => {
+  if (document.getElementsByClassName("col-md-4").length == 0) {
+    setTimeout(() => {
+      displayInfo();
+    }, 3000);
+  } else {
+    console.log(document.getElementsByClassName("col-md-4")[0]);
+    const playerRightSideDetails =
+      document.getElementsByClassName("col-md-4")[0];
+    const teamStatSection = document.createElement("div");
+    teamStatSection.innerHTML = "Hello";
+    playerRightSideDetails.prepend(teamStatSection);
+  }
 };
 
-const tempFuntion = async () => {
-  document.body.style.border = "5px solid red";
-};
-
-document.addEventListener("DOMContentLoaded", (event) => {
-  console.log("im gonna blow my head off");
-});
 document.body.style.border = "5px solid red";
-console.log("Help me Please");
 displayInfo();
