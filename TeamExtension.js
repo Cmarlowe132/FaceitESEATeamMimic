5c39d37190dd063e4e3a663ef6e5034ef32ffb33
Carter Marlowe <marlowc@email.sc.edu>
2023-09-17 21:48:40 -0400

    Fixing small bug with passing player name

--- TeamExtension.js
+++ TeamExtension.js
@@ -1,21 +1,20 @@
 browser.runtime.onMessage.addListener((obj, sender, response) => {
-  if (obj.playerName) {
-    console.log(typeof obj.playerName);
-    displayInfo(obj.playerName);
+  let name = obj.playerName;
+  if (name) {
+    displayInfo(name);
   }
 });
 
 const displayInfo = (playerName) => {
   if (document.getElementsByClassName("col-md-4").length == 0) {
     setTimeout(() => {
-      displayInfo();
+      displayInfo(playerName);
     }, 1000);
   } else {
-    console.log(playerName);
     const playerRightSideDetails =
       document.getElementsByClassName("col-md-4")[0];
     const teamStatTitle = document.createElement("h3");
-    teamStatTitle.innerHTML = "Faceit Team";
+    teamStatTitle.innerHTML = "Team Stats";
     const teamStatSection = document.createElement("div");
     teamStatSection.className = "profile__block__content";
     teamStatSection.innerHTML = playerName;
