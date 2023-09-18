3ccf1b521eab4d946e3b64f7c1bfd95e4e382bb7
Carter Marlowe <marlowc@email.sc.edu>
2023-09-17 21:13:41 -0400

    Adding the team stat section to the player's profile page

--- TeamExtension.js
+++ TeamExtension.js
@@ -9,16 +9,20 @@ const displayInfo = () => {
   if (document.getElementsByClassName("col-md-4").length == 0) {
     setTimeout(() => {
       displayInfo();
-    }, 3000);
+    }, 1000);
   } else {
     console.log(document.getElementsByClassName("col-md-4")[0]);
     const playerRightSideDetails =
       document.getElementsByClassName("col-md-4")[0];
+    const teamStatTitle = document.createElement("h3");
+    teamStatTitle.innerHTML = "Faceit Team";
     const teamStatSection = document.createElement("div");
+    teamStatSection.className = "profile__block__content";
     teamStatSection.innerHTML = "Hello";
+
     playerRightSideDetails.prepend(teamStatSection);
+    playerRightSideDetails.prepend(teamStatTitle);
   }
 };
 
-document.body.style.border = "5px solid red";
 displayInfo();
