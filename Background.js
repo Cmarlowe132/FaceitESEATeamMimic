30c8f8b3a83f3b30e73d577f043e5215a7f06072
Carter Marlowe <marlowc@email.sc.edu>
2023-09-21 17:12:36 -0400

    Added API call to get a player's ID

--- Background.js
+++ Background.js
@@ -1,8 +1,14 @@
+const faceitAPIURL = "https://open.faceit.com/data/v4/";
+const headerAccept = "application/json";
+const headerAuthorization = "Bearer 229b8d49-00be-4d43-8170-f4ee80873973";
+let playerID = "";
+
 browser.tabs.onUpdated.addListener((tabId, tab) => {
   if (tab.url && tab.url.includes("players")) {
     const urlName = tab.url.split("players/")[1];
     if (!urlName.includes("/")) {
       const playerName = urlName;
+      console.log(callAPIs(playerName));
       console.log(playerName);
       browser.tabs.sendMessage(tabId, {
         playerName: playerName,
@@ -11,6 +17,20 @@ browser.tabs.onUpdated.addListener((tabId, tab) => {
   }
 });
 
-const callAPIs = async (username) => {};
-
-//229b8d49-00be-4d43-8170-f4ee80873973 faceit api key
+const callAPIs = async (username) => {
+  const response = await fetch(
+    faceitAPIURL +
+      "search/players?nickname=" +
+      username +
+      "&game=csgo&offset=0&limit=20",
+    {
+      method: "GET",
+      headers: {
+        accept: headerAccept,
+        Authorization: headerAuthorization,
+      },
+    }
+  );
+  const values = await response.json();
+  playerID = values.items[0].player_id;
+};
